364a042517c9759744c7b91651a853e2b1356492
jaykhan-dev <jaykhan.sound@gmail.com>
2022-11-05 16:12:58 -0400

    starting affiliate gsap project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App bg-black h-screen grid place-items-center text-white">
+      <h1>Bitcoin Affiliate Site</h1>
+    </div>
+  )
+}
+
+export default App
